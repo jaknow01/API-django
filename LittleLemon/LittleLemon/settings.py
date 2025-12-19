f7096560c02ed78784fcc71bb1d305a3f41a38de
Jakub_Nowakowski <jakub.nowakowski@mim.ai>
2025-12-19 15:26:19 +0100

    more settings for the drf

--- LittleLemon/LittleLemon/settings.py
+++ LittleLemon/LittleLemon/settings.py
@@ -38,7 +38,9 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
-    'LittleLemonAPI'
+    'rest_framework.authtoken',
+    'LittleLemonAPI',
+    'djoser',
 ]
 
 MIDDLEWARE = [
@@ -119,5 +121,21 @@ USE_TZ = True
 STATIC_URL = 'static/'
 
 REST_FRAMEWORK = {
-    
+    "DEFAULT_PAGINATION_CLASS": [
+        "rest_framework.pagination.PageNumberPagination"
+    ],
+    "PAGE_SIZE": 10,
+    "DEFAULT_RENDERER_CLASSES": [
+        "rest_framework.renderers.JSONRenderer",
+        "rest_framework.renderers.BrowsableAPIRenderer",
+        "rest_framework_xml.renderers.XMLRenderer",
+    ],
+    "DEFAULT_AUTHENTICATION_CLASSES" : [
+        "rest_framework.authentication.TokenAuthentication",
+        "rest_framework.authentication.SessionAuthentication",
+    ],
+}
+
+DJOSER = {
+    "USER_ID_FIELD": "username"
 }
