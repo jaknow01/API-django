8ba1d06a30acc1c9d4bcce1dc45dbd69c0c94ca2
Jakub_Nowakowski <jakub.nowakowski@mim.ai>
2026-01-05 10:35:56 +0100

    actually tried to launch

--- LittleLemon/LittleLemonAPI/migrations/0002_create_groups.py
+++ LittleLemon/LittleLemonAPI/migrations/0002_create_groups.py
@@ -1,6 +1,6 @@
 from django.db import migrations
 
-def create_user_groups(apps):
+def create_user_groups(apps, schema_editor):
     Group = apps.get_model('auth', 'Group')
     Group.objects.get_or_create(name='managers')
     Group.objects.get_or_create(name='delivery')
@@ -10,6 +10,7 @@ class Migration(migrations.Migration):
 
     dependencies = [
         ('LittleLemonAPI', '0001_initial'),
+        ('auth', '__latest__'),
     ]
 
     operations = [
--- LittleLemon/LittleLemonAPI/urls.py
+++ LittleLemon/LittleLemonAPI/urls.py
@@ -2,13 +2,13 @@ from django.urls import path
 from .views import MenuItemsView, GroupManagementView, GroupDeliveryView, CartView
 
 urlpatterns = [
-    path("/menu-items", MenuItemsView.as_view(
+    path("menu-items", MenuItemsView.as_view(
         {
             'get': 'list',
             'post': 'create'
         }
     )),
-    path("/menu-items/<int:pk>", MenuItemsView.as_view(
+    path("menu-items/<int:pk>", MenuItemsView.as_view(
         {
             'get': 'retrieve',
             'put': 'update',
@@ -16,35 +16,35 @@ urlpatterns = [
             'delete': 'destroy',
         }
     )),
-    path('/groups/manager/users', GroupManagementView.as_view(
+    path('groups/manager/users', GroupManagementView.as_view(
         {
             'get': 'list',
             'post': 'create'
         }
     )),
-    path('/groups/manager/users/<int:pk>', GroupManagementView.as_view(
+    path('groups/manager/users/<int:pk>', GroupManagementView.as_view(
         {
             'delete': 'destroy'
         }
     )),
-    path('/groups/delivery-crew/users', GroupDeliveryView.as_view(
+    path('groups/delivery-crew/users', GroupDeliveryView.as_view(
         {
             'get': 'list',
             'post': 'create'
         }
     )),
-    path('/groups/delivery-crew/users/<int:pk>', GroupDeliveryView.as_view(
+    path('groups/delivery-crew/users/<int:pk>', GroupDeliveryView.as_view(
         {
             'delete': 'destroy'
         }
     )),
-    path('/cart/manu-items', CartView.as_view(
+    path('cart/menu-items', CartView.as_view(
         {
             'get': 'list',
             'post': 'create',
             'delete': 'destroy'
         }
     )),
-    path('/orders'),
-    path('/orders/<int:pk>')
+    # path('/orders'),
+    # path('/orders/<int:pk>')
 ]
